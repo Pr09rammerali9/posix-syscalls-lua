3522fe2409acba667135b9385aec726fbfecb54a
Pr09rammerali9 <hakamiali197@gmail.com>
2025-07-31 03:01:15 +0300

    Create makefile.sh

---
+++ makefile.sh
@@ -0,0 +1,17 @@
+Cc=clang
+flgs=-fPIC -shared
+cf=main.c
+libobj=lua-posix-syscalls.so
+libpath=/usr/lib
+if [[ $1 == "build" ]]; then
+
+$Cc $flgs $cf -o $libobj
+
+elif [[ $1 == "clean" ]]; then 
+
+rm $libobj
+
+elif [[ $1 == "mv" ]]; then 
+
+mv $libobj $libpath/$libobj
+fi
